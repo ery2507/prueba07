0308524fd3c56dc71fe97f1c738575b33fabb17d
erika <ery2507.ec@gmail.com>
2013-11-21 16:31:05 -0600

    insertar
    funcion insertar y se agregó la pagina2 del listado

--- js/acciones.js
+++ js/acciones.js
@@ -1,45 +1,54 @@
 // JavaScript Document
-$(document).ready(function(e) {
-	 document.addEventListener("deviceready",function(){		 
-
-//se crea la base de datos Test es el nombre de la bd, 1.0 la version y 65535 el		 tamaño em bytes
-var db = openDatabase ("Test", "1.0", "Base de Prueba", 65535);
-
-$("#Crear").bind ("click", function (event)
+$(document).ready(function(e) 
 {
+  document.addEventListener("deviceready",function()
+  {		 
+	//se crea la base de datos Test es el nombre de la bd, 1.0 la version y 65535 el tamaño en bytes	
+	var db = openDatabase ("Test", "1.0", "Base de Prueba", 65535);
+	
+	$("#Crear").bind ("click", function (event)
+	 {			
+		db.transaction (function (ejecutar) 
+		  {
+   			 var sql = "CREATE TABLE Clientes (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, nombre VARCHAR(100) NOT NULL, apellido VARCHAR(100) NOT NULL)";
+    		ejecutar.executeSql (sql, undefined, function () 
+			{ 
+      			alert ("Tabla creada");
+    		}, error);//executesql
+ 		 });//Ejecutar
+	  });//Crear
 
-				
-  db.transaction (function (ejecutar) 
-  {
-    var sql = "CREATE TABLE Clientes (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, nombre VARCHAR(100) NOT NULL, apellido VARCHAR(100) NOT NULL)";
-    ejecutar.executeSql (sql, undefined, function ()
-    { 
-      alert ("Tabla creada");
-    }, error);
-  });
-});
+	$("#Eliminar").bind ("click", function (event)
+	 {
+ 		if (!confirm ("Borrar tabla?", "")) return;
+  		db.transaction (function (transaction) 
+		{
+    	   var sql = "DROP TABLE Clientes";
+    	   transaction.executeSql (sql, undefined,function ()
+		   { 
+      			alert ("Tabla Borrada");
+    		} , error);//executesql
+  		});//transaction
+	 });//Eliminar
 
-$("#Eliminar").bind ("click", function (event)
-{
-  if (!confirm ("Borrar tabla?", "")) return;
-  db.transaction (function (transaction) 
-  {
-    var sql = "DROP TABLE Clientes";
-    transaction.executeSql (sql, undefined,function ()
-    { 
-      alert ("Tabla Borrada");
-    } , error);
-  });
-});
-
-
-
-
-function error (transaction, err) 
-{
-  alert ("Error de Base de Datos : " + err.message);
-  return false;
-}    
-	 }, false);
-});
+	function error (transaction, err) {
+ 	 alert ("Error de Base de Datos : " + err.message);
+  	 return false;
+	}//funcion error 
+	
+	$("#Insertar").bind ("click", function (event)
+	{
+ 	   var v_nombre = $("#nombre").val ();
+ 	   var v_apellido = $("#apellido").val ();
+  	   db.transaction (function (ejecutar) 
+  		{
+    		var sql = "INSERT INTO Clientes (nombre, apellido) VALUES (?, ?)";
+    		ejecutar.executeSql (sql, [v_nombre, v_apellido], function ()
+    		{ 
+     		 alert ("Cliente Agregado");
+    		}, error);//executesql
+ 		 });//ejecutar
+	});  //insertar
+  }, false);//deviceready
+});//document
 
