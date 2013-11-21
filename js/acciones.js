89fbadf769e490edb5fba2ac6c657a8546c5c55f
erika <ery2507.ec@gmail.com>
2013-11-21 15:44:46 -0600

    error
    bd con funcion error

--- js/acciones.js
+++ js/acciones.js
@@ -1,25 +1,45 @@
 // JavaScript Document
+$(document).ready(function(e) {
+	 document.addEventListener("deviceready",function(){		 
 
-$(document).ready(function(e){// e de evento
-	//se crea la base de datos Test es el nombre de la bd, 1.0 la version y 65535 el		 tamaño em bytes
-	var db=OpenDataBase("Test","1.0","Test", 65535);
-	document.addEventListener("deviceready",function(){
-		$('#Crear').bind("Click", function(event){
-			db.transaction(function(ejecutar){
-				var SQL="CREATE TABLE Clientes id INTEGER NOT NULL PRIMARY KEYGEN, nombre VARCHAR(64) NOT NULL, apellido VARCHAR(100)";
-				ejecutar.executeSql(SQL,undefined, function(){
-					navigator.notification.alert("Tabla Creada");
-				}, error);
-					});//Ejecutar
-			});//Crear
-		$('Eliminar').bind("Click", function (event){
-			if(!Navigator.Notification.Confirm("Borrar Tabla?",""))
-			 	return;
-				db.transaction(function(ejecutar){
-					var SQL="DROP TABLE Clientes";
-					navigator.notification.alert("Tabla Borrada");
-				},error);
-			});//Eliminar
-		}, false);//add
-		
-});// ready
+//se crea la base de datos Test es el nombre de la bd, 1.0 la version y 65535 el		 tamaño em bytes
+var db = openDatabase ("Test", "1.0", "Base de Prueba", 65535);
+
+$("#Crear").bind ("click", function (event)
+{
+
+				
+  db.transaction (function (ejecutar) 
+  {
+    var sql = "CREATE TABLE Clientes (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, nombre VARCHAR(100) NOT NULL, apellido VARCHAR(100) NOT NULL)";
+    ejecutar.executeSql (sql, undefined, function ()
+    { 
+      alert ("Tabla creada");
+    }, error);
+  });
+});
+
+$("#Eliminar").bind ("click", function (event)
+{
+  if (!confirm ("Borrar tabla?", "")) return;;
+  db.transaction (function (transaction) 
+  {
+    var sql = "DROP TABLE Clientes";
+    transaction.executeSql (sql, undefined,function ()
+    { 
+      alert ("Tabla Borrada");
+    } , error);
+  });
+});
+
+
+
+
+function error (transaction, err) 
+{
+  alert ("Error de Base de Datos : " + err.message);
+  return false;
+}    
+	 }, false);
+});
+
